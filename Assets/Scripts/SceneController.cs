37bd0177d6f52818201178accaa69d804b3010e6
Guillermo Diéguez <gdieguez@silentiumapps.com>
2020-02-13 17:56:18 -0300

    .

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -99,13 +99,9 @@ public class SceneController : MonoBehaviour
 
 	public void FinishEditing()
 	{
-		// disable touch scripts on current item
 		CurrentItem.GetComponent<LeanPinchScale>().enabled = false;
 		CurrentItem.GetComponent<LeanTwistRotateAxis>().enabled = false;
-
-		// override position controls
 		modelController.model = null;
-
 		CloseControls();
 	}
 
@@ -117,37 +113,26 @@ public class SceneController : MonoBehaviour
 
 	public void OnCatalogItemClicked(string prefabName)
 	{
-		//load and instantiate selected prefab
 		GameObject prefab = Resources.Load($"Prefabs/{prefabName}") as GameObject;
 		CurrentItem = Instantiate(prefab);
 
-		//show tooltip
 		GameObject.Find("ToolTip").GetComponent<Animator>().SetTrigger("ShouldShow");
 	}
 
 	private void SetUpCurrentItem()
 	{
-		// make prefab a children of ground scene
 		CurrentItem.transform.parent = GameObject.Find("Ground Plane Stage").transform;
-
-		// set x, y, z of instantiated item
 		CurrentItem.transform.localPosition = new Vector3(0, 0, 0);
-
-		// set scale of instantiated item
 		CurrentItem.transform.localScale = new Vector3(1, 1, 1);
 
-		// disable movement scripts on previous item if exists
 		if (previousItem != null)
 		{
 			previousItem.GetComponent<LeanPinchScale>().enabled = false;
 			previousItem.GetComponent<LeanTwistRotateAxis>().enabled = false;
 		}
 
-		// add movement scripts to current item
 		CurrentItem.AddComponent<LeanPinchScale>();
 		CurrentItem.AddComponent<LeanTwistRotateAxis>();
-
-		// override position controls
 		modelController.model = CurrentItem;
 	}
 }
