3bf2dc64817a5f9a2fe9d25ac3619d599a87e460
Guillermo Diéguez <gdieguez@silentiumapps.com>
2020-03-02 17:59:50 -0300

    Tweaks MeasureScene

--- Assets/Scripts/Controllers/MeasureSceneController.cs
+++ Assets/Scripts/Controllers/MeasureSceneController.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Scripts.Models;
+using Assets.Scripts.Support;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -21,6 +23,7 @@ namespace Assets.Scripts.Controllers
 		private GameObject PreviousMarker;
 		private GameObject groundPlane;
 		private GameObject planeFinder;
+		private bool shouldUpdateMetrics = false;
 		private Color lineRendererMaterial;
 
 		public List<Marker> MarkerList { get; set; }
@@ -55,11 +58,17 @@ namespace Assets.Scripts.Controllers
 
 		void Update()
 		{
-			if (MarkerList != null && MarkerList.Count > 1)
+			if (shouldUpdateMetrics && MarkerList != null && MarkerList.Count > 1)
 			{
-				UpdateMetrics();
+				StartCoroutine(UpdateMetrics());
+			}
+			else
+			{
+				StopCoroutine(UpdateMetrics());
 			}
 		}
+
+
 
 		private void CreateLineRenderer(Vector3 initialPos, Vector3 finalPos)
 		{
@@ -84,35 +93,30 @@ namespace Assets.Scripts.Controllers
 			}
 		}
 
-		private void UpdateMetrics()
+		IEnumerator UpdateMetrics()
 		{
-			try
+			// last segment distance
+			var lastSegmentDistance = MarkerList.Last().LastSegmentDistance;
+			debugText.text = lastSegmentDistance > 1 ? $"LAST: {Math.Round(lastSegmentDistance, 2)} mts." : $"LAST: {Math.Round(lastSegmentDistance, 2) * 100} cms.";
+
+			// total distance
+			var perimeterDistance = 0.0f;
+			foreach (var marker in MarkerList)
 			{
-				// last segment distance
-				var lastSegmentDistance = MarkerList.Last().LastSegmentDistance;
-				debugText.text = lastSegmentDistance > 1 ? $"LAST: {Math.Round(lastSegmentDistance, 2)} mts." : $"LAST: {Math.Round(lastSegmentDistance, 2) * 100} cms.";
-
-				// total distance
-				var perimeterDistance = 0.0f;
-				foreach (var marker in MarkerList)
-				{
-					var dist = marker.LastSegmentDistance;
-					perimeterDistance += dist;
-				}
-
-				debugText.text += perimeterDistance > 1 ? $" TOTAL: {Math.Round(perimeterDistance, 2)} mts." : $" TOTAL: {Math.Round(perimeterDistance, 2) * 100} cms.";
-
-				// angle
-				var from = CurrentMarker.transform.position - PreviousMarker.transform.position;
-				var to = Vector3.ProjectOnPlane(from, -CurrentMarker.transform.up);
-				var angle = Vector3.Angle(from, to);
-
-				debugText.text += $" ANGLE: {Math.Round(angle, 2)} dgs.";
+				var dist = marker.LastSegmentDistance;
+				perimeterDistance += dist;
 			}
-			catch (Exception ex)
-			{
-				debugText.text = $"{ex.Message}";
-			}
+
+			debugText.text += perimeterDistance > 1 ? $" TOTAL: {Math.Round(perimeterDistance, 2)} mts." : $" TOTAL: {Math.Round(perimeterDistance, 2) * 100} cms.";
+
+			// angle
+			var from = CurrentMarker.transform.position - PreviousMarker.transform.position;
+			var to = Vector3.ProjectOnPlane(from, -CurrentMarker.transform.up);
+			var angle = Vector3.Angle(from, to);
+
+			debugText.text += $" ANGLE: {Math.Round(angle, 2)} dgs.";
+
+			yield return new WaitForSeconds(.1f);
 		}
 
 		public void ResetScene()
@@ -144,7 +148,7 @@ namespace Assets.Scripts.Controllers
 				}
 
 				MarkerList.Add(newMarker);
-
+				shouldUpdateMetrics = true;
 			}
 			catch (Exception ex)
 			{
@@ -154,30 +158,84 @@ namespace Assets.Scripts.Controllers
 
 		public void FinishAddingMarkers()
 		{
-			var lastMarker = new Marker()
+			try
 			{
-				Previous = MarkerList.Last(),
-				Position = MarkerList.First().Position,
-			};
+				shouldUpdateMetrics = false;
 
-			MarkerList.Add(lastMarker);
+				var lastMarker = new Marker()
+				{
+					Previous = MarkerList.Last(),
+					Position = MarkerList.First().Position,
+				};
 
-			Vector3 initialPos = MarkerList.Last().Position;
-			Vector3 finalPos = MarkerList.First().Position;
-			CreateLineRenderer(initialPos, finalPos);
+				Vector3 initialPos = MarkerList.Last().Position;
+				Vector3 finalPos = MarkerList.First().Position;
+				CreateLineRenderer(initialPos, finalPos);
 
-			CreateMesh();
+				MarkerList.Add(lastMarker);
+				CreateMesh();
+			}
+			catch (Exception ex)
+			{
+				debugText.text = ex.Message;
+				Debug.Log(ex);
+			}
 		}
 
 		private void CreateMesh()
 		{
+			var vectorList = new List<Vector2>();
+
+			foreach (Marker marker in MarkerList)
+			{
+				vectorList.Add(new Vector2(marker.Position.x, marker.Position.y));
+			}
+
+			Vector2[] vertices2D = vectorList.ToArray();
+
+			//Vector2[] vertices2D = new Vector2[]
+			//{
+			//	new Vector2(0,0),
+			//	new Vector2(0,50),
+			//	new Vector2(50,50),
+			//	new Vector2(50,100),
+			//	new Vector2(0,100),
+			//	new Vector2(0,150),
+			//	new Vector2(150,150),
+			//	new Vector2(150,100),
+			//	new Vector2(100,100),
+			//	new Vector2(100,50),
+			//	new Vector2(150,50),
+			//	new Vector2(150,0),
+			//};
+
+			Triangulator.Instance.SetPoints(vertices2D);
+			int[] indices = Triangulator.Instance.Triangulate();
+
+			Vector3[] vertices = new Vector3[vertices2D.Length];
+			for (int i = 0; i < vertices.Length; i++)
+			{
+				vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, MarkerList[i].Position.z);
+			}
+
+			Mesh msh = new Mesh();
+			msh.vertices = vertices;
+			msh.triangles = indices;
+			msh.RecalculateNormals();
+			msh.RecalculateBounds();
+
+			GameObject emptyGo = new GameObject();
+			emptyGo.name = "Area";
+			emptyGo.transform.parent = groundPlane.transform;
+			emptyGo.transform.localPosition = new Vector3(MarkerList.First().Position.x, 0.06f, MarkerList.First().Position.z);
+
+			MeshRenderer meshRenderer = emptyGo.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+			meshRenderer.enabled = true;
+
+			MeshFilter filter = emptyGo.AddComponent(typeof(MeshFilter)) as MeshFilter;
+			filter.mesh = msh;
+
 			debugText.text = "CREATED MESH!";
-			// crear array de vertices
-			//Vector2[] vertices2D = ARMarkerList.ForEach(marker => marker.transform.pos
-			// usar triangulator para calcular indices de triangulos
-			// crear los vertices
-			// crear la malla
-			// crear gameobject con la malla
 		}
 
 		public void InitializeDefaultScene()
