753427a77c48603d3d874036f4d78abc8f8e955e
Guillermo Diéguez <gdieguez@silentiumapps.com>
2020-03-13 12:15:15 -0300

    Created GalleryController

---
+++ Assets/Scripts/Controllers/GallerySceneController.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GallerySceneController : MonoBehaviour
+{
+	[SerializeField]
+	GameObject canvas;
+	string[] files = null;
+	int whichScreenShotIsShown = 0;
+
+	public void GoMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+	}
+
+	void Start()
+	{
+		Screen.orientation = ScreenOrientation.Landscape;
+
+		files = Directory.GetFiles(Application.persistentDataPath + "/", "*.png");
+		if (files.Length > 0)
+		{
+			GetPictureAndShowIt();
+		}
+	}
+
+	void GetPictureAndShowIt()
+	{
+		string pathToFile = files[whichScreenShotIsShown];
+		Texture2D texture = GetScreenshotImage(pathToFile);
+		Sprite sp = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+			new Vector2(0.5f, 0.5f));
+		canvas.GetComponent<Image>().sprite = sp;
+	}
+
+	Texture2D GetScreenshotImage(string filePath)
+	{
+		Texture2D texture = null;
+		byte[] fileBytes;
+		if (File.Exists(filePath))
+		{
+			fileBytes = File.ReadAllBytes(filePath);
+			texture = new Texture2D(2, 2, TextureFormat.RGB24, false);
+			texture.LoadImage(fileBytes);
+		}
+		return texture;
+	}
+
+
+	public void LoadPreviousScreenshot()
+	{
+		if (files.Length > 0)
+		{
+			whichScreenShotIsShown -= 1;
+			if (whichScreenShotIsShown < 0)
+				whichScreenShotIsShown = files.Length - 1;
+			GetPictureAndShowIt();
+		}
+	}
+	public void LoadNextScreenshot()
+	{
+		if (files.Length > 0)
+		{
+			whichScreenShotIsShown += 1;
+			if (whichScreenShotIsShown > files.Length - 1)
+				whichScreenShotIsShown = 0;
+			GetPictureAndShowIt();
+		}
+	}
+}
